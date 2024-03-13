cd570b143efd211e5d73e142a7d8d4d81da447b2
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2024-03-14 02:11:18 +0300

    fix(go-backend): updated db schema

--- go-backend/migrations/schema.sql
+++ go-backend/migrations/schema.sql
@@ -1,10 +1,9 @@
-alter database cbr set timezone to 'Europe/Moscow';
-
+alter database cbr
+set timezone to 'Europe/Moscow';
 create table if not exists session(
     id uuid primary key,
     created_at timestamp default current_timestamp
 );
-
 create table if not exists query(
     id bigserial primary key,
     fk_session_id uuid references session(id),
@@ -13,7 +12,6 @@ create table if not exists query(
     user_agent text not null,
     created_at timestamp default current_timestamp
 );
-
 create table if not exists response(
     id bigserial primary key,
     fk_session_id uuid references session(id),
@@ -22,17 +20,14 @@ create table if not exists response(
     context jsonb not null,
     created_at timestamp default current_timestamp
 );
-
 create table if not exists "user"(
     id bigserial primary key,
     email text unique not null,
     password text not null,
     created_at timestamp default current_timestamp
 );
-
 create table if not exists users_sessions(
     fk_user_id bigint references "user"(id),
     fk_session_id uuid references session(id),
-    created_at timestamp default current_timestamp,
-    primary key (fk_user_id, fk_session_id)
+    created_at timestamp default current_timestamp
 );
