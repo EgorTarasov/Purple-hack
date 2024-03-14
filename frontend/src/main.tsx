19fe7c611caa3b11d09fb22ca0b0509250bf4fbd
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2024-03-14 09:13:10 +0300

    wip(frontend): dockefile for frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -8,9 +7,9 @@ import { AuthProvider } from "./context/Authprovider.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
 		{/* <React.StrictMode> */}
-			<AuthProvider>
-				<App />
-			</AuthProvider>
+		<AuthProvider>
+			<App />
+		</AuthProvider>
 		{/* </React.StrictMode> */}
 	</BrowserRouter>
 );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,15 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [react()],
+	server: {
+		port: 3000,
+	},
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
